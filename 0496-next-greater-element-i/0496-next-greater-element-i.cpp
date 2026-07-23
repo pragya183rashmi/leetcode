39141c3e77ed02a646ab7d2e841726ce1d367b78
Pragya Rashmi <pragyarashmi183@gmail.com>
2026-07-23 14:15:57 +0530

    Time: 7 ms (10.62%), Space: 12.8 MB (39.95%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+
+        unordered_map<int, int> mp;
+        stack<int> st;
+
+        // Find next greater element for every element in nums2
+        for (int i = 0; i < nums2.size(); i++) {
+
+            while (!st.empty() && st.top() < nums2[i]) {
+                mp[st.top()] = nums2[i];
+                st.pop();
+            }
+
+            st.push(nums2[i]);
+        }
+
+        // Remaining elements have no next greater element
+        while (!st.empty()) {
+            mp[st.top()] = -1;
+            st.pop();
+        }
+
+        vector<int> ans;
+
+        // Get answers for nums1
+        for (int i = 0; i < nums1.size(); i++) {
+            ans.push_back(mp[nums1[i]]);
+        }
+
+        return ans;
+    }
+};
